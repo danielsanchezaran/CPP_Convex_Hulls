81fdcf0f2e8fb22eb13eb492b0d9382994c44168
daniel_sanchez <danielsanchezaranja@gmail.com>
2022-11-13 14:38:47 +0900

    [manual testing] Points inside or outside chull

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,5 +1,3 @@
-//This code follows the Google C++ Style guide
-
 #include <json.hpp>
 #include <fstream>
 #include <iostream>
@@ -16,7 +14,25 @@ int main()
     std::cout << data["convex hulls"].size() <<"\n";
 
     std::vector<ConvexHull> convex_hull_v;
-    convex_hull_v = convexHullFromJson(data);
-    std::cout << convex_hull_v[0].apex[0];
+    convex_hull_v = convexHullsFromJson(data);
+    std::cout << convex_hull_v[0].apex[0] <<"\n";
+
+    std::vector<Point> vertex;
+    Point P1(-1,1);
+    Point P2(1,1);
+    Point P3(1,-1);
+    Point P4(-1,-1);
+    vertex.push_back(P1);
+    vertex.push_back(P2);
+    vertex.push_back(P3);
+    vertex.push_back(P4);
+
+    ConvexHull C(vertex,0);
+
+    Point Pin(0,0);
+    Point Pout(1.01 ,1);
+
+    std::cout << "Pin Inside " << C.isPointInside(Pin) << "\n";
+    std::cout << "Pout Inside " << C.isPointInside(Pout) << "\n";
     return 0;
 }
