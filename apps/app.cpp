d849bf1ff5b9596c4bc1519afada438de6659087
daniel_sanchez <danielsanchezaranja@gmail.com>
2022-11-15 23:14:42 +0900

    [feat] app does the required challenge

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,77 +1,22 @@
-#include <json.hpp>
+// Using the google c++ style guide
 #include <fstream>
 #include <iostream>
 #include <convex_hull.hpp>
+#include <json.hpp>
 
 using json = nlohmann::json;
 
 int main()
 {
-
     std::ifstream f("../convex_hulls.json");
     json data = json::parse(f);
-    // std::cout << data << "\n";
-    std::cout << data["convex hulls"].size() << "\n";
-
     std::vector<ConvexHull> convex_hull_v;
     convex_hull_v = convexHullsFromJson(data);
-    std::cout << convex_hull_v[0].apex[0] << "\n";
-
     double overlap = 0.5;
-    std::vector<ConvexHull> output = eliminateOverlappingCHulls(convex_hull_v, overlap);
+    std::vector<ConvexHull> remaining_c_hulls = eliminateOverlappingCHulls(convex_hull_v, overlap);
+    json remaining_c_hulls_json = convexHullsToJson(remaining_c_hulls);
 
-    for (int i = 0; i < output.size();++i){
-        std::cout << "Chull ID " << output[i].id <<"\n";
-    }
-
-    std::vector<Point> vertex;
-    Point P1(-1, -1);
-    Point P2(-1, 1);
-    Point P3(1, 1);
-    Point P4(1, -1);
-    vertex.push_back(P1);
-    vertex.push_back(P2);
-    vertex.push_back(P3);
-    vertex.push_back(P4);
-
-    ConvexHull C(vertex, 0);
-
-    std::vector<Point> vertex2;
-    Point P12(0, 0);
-    Point P22(-0.5, -1.1);
-    Point P32(0.5, -1.1);
-    vertex2.push_back(P12);
-    vertex2.push_back(P22);
-    vertex2.push_back(P32);
-
-    ConvexHull C2(vertex2, 0);
-    std::vector<Point> vertex3;
-    ConvexHull interC(vertex, 0);
-    bool intersection = getIntersectingPolygon(C, C2, interC);
-    std::cout << "they intersect: " << intersection << "\n";
-    std::cout << "inner polygon is: \n";
-    if (intersection)
-    {
-        for (Point p : interC.apex)
-        {
-            std::cout << p << "\n";
-        }
-    }
-
-    Point Pin(0, 0);
-    Point Pout(1, 1);
-    Point P3_(0.5, 0);
-    Point P4_(0.5, 1);
-
-    std::cout << "Pin Inside " << C.isPointInside(Pin) << "\n";
-    std::cout << "Pout Inside " << C.isPointInside(Pout) << "\n";
-
-    Line L1(Pin, Pout);
-    Line L2(P3_, P4_);
-    Point inter;
-    double eps = 0.00001;
-    std::cout << "intersect: " << segmentsIntersect(L1, L2, inter, eps) << "\n";
-    std::cout << "inter point " << inter << "\n";
-
+    std::ofstream file("result_convex_hulls.json");
+    file << std::setw(4) << remaining_c_hulls_json << std::endl;
     return 0;
 }
